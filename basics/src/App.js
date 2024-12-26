8c6ced377e85f4c2ac37ad1d63edc82c49c35548
akib10khan <zerx789@gmail.com>
2024-12-26 12:53:04 -0500

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Greeting from './Greeting';
+
 function App() {
+  let name = 'Akib';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello from React Js</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'}/>
+    {/* <Home name = {'Akib'} age = {24} />
+    <About />
+    <Contact /> */}
     </div>
   );
 }
---
+++ basics/src/Greeting.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Greeting(props) {
+    return (
+        <h3>Hello {props.name}</h3>
+    )
+}
